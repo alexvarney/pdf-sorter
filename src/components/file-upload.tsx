4b2092e27edf7edc38c20ccb72fbe2ab9a13a2c4
Jerrin Thomas <Jthomas0668@conestogac.on.ca>
2022-12-07 11:07:40 -0500

    add file upload desc

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -8,6 +8,7 @@ const DropContainer = styled.div`
   width: clamp(250px, 60vw, 768px);
   height: 256px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: var(--bg-light-grey);
@@ -15,6 +16,11 @@ const DropContainer = styled.div`
   //dashed border
   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='rgba(0,0,0,0.15)' stroke-width='3' stroke-dasharray='8%2c 16%2c 8%2c 16' stroke-dashoffset='3' stroke-linecap='square'/%3e%3c/svg%3e");
   border-radius: 16px;
+
+  & > h6 {
+    font-weight: lighter;
+    margin: 1em;
+  }
 `;
 
 const StyledButton = styled(Button)`
@@ -23,7 +29,7 @@ const StyledButton = styled(Button)`
 
   & > span {
     margin-right: 0.125rem;
-  }
+  } 
 `;
 
 type UploadHandler = (values: PDFUpload[]) => void;
@@ -61,6 +67,7 @@ export const FileUploader = ({ onUpload }: { onUpload: UploadHandler }) => {
           <StyledButton type="dashed" name="file-upload">
             <span>Upload PDFs</span> <VscFilePdf></VscFilePdf>
           </StyledButton>
+          <h6>or drag and drop files here.</h6>
         </DropContainer>
       </Upload>
     </>
